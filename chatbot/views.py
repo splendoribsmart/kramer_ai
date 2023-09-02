aa927c87f2f6380f99b3e3c9a038ac1eeda7c4c9
Zooty <splendoribsmart@gmail.com>
2023-09-02 07:09:37 +0100

    Frist Local Commit - fully fuctional untrained ai chatbot for karmer website

---
+++ chatbot/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import openai
+
+from django.utils import timezone
+
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+    )
+    
+    print(response)
+    answer = response.choices[0].message.content.strip()
+    return answer
+
+# Create your views here.
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message': message, 'response': response})
+    return render(request, 'chatbot.html', {})
+
